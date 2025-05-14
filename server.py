3986364dd035770e81ca70bba99632b675bf2e1c
DOMBALLE <theodom974>
2025-05-14 10:27:12 +0200

    projet

--- server.py
+++ server.py
@@ -124,4 +124,6 @@ def get_image(filename):
 # ---------------------- LANCEMENT ----------------------
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
